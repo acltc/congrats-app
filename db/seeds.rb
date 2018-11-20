68e844f8438bdb29872ec17cb32eb064ba637deb
Jay Wengrow <jaywngrw@gmail.com>
2018-11-20 10:04:50 -0600

    tweaking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(name: 'Jay Wengrow', video_1: '6619d2c659894a20b4fdcb8786c2b695', video_2: '71752e50622f49168d5330b56d6c3441', video_3: '46625415bbcc4e52b1599564d7046903', slug: 'test')
